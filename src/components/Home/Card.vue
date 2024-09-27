2a60562ca00aabd2fa78d31ee1fdb5f995b1e0c5
Hernany Simao <hernanysimao123@gmail.com>
2024-09-27 16:34:46 +0100

    feat: preparei a estrutura dos cards

--- src/components/Home/Card.vue
+++ src/components/Home/Card.vue
@@ -1,5 +1,25 @@
+<script setup>
+</script>
+
 <template>
   <section>
-    <h1>Card</h1>
+    <div class="container">
+      <div class="row">
+        <div v-for="i in 6" :key="i" class="col-md-4 mb-4">
+          <div class="card">
+            <div class="card-body">
+              <div class="d-flex justify-content-center">
+                <img src="" alt="image" />
+              </div>
+              <div class="text-center">
+                <span>Nª 123</span>
+                <h2>Nome</h2>
+                <button>Tipo</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
