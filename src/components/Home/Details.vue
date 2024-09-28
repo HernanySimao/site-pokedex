036b2d20b0bd87bbe64c35ad223a9e30e93fe014
Hernany Simao <hernanysimao123@gmail.com>
2024-09-28 18:25:52 +0100

    feat: dei tratamento nos detalhes do pokemon

--- src/components/Home/Details.vue
+++ src/components/Home/Details.vue
@@ -1,26 +1,26 @@
 <script setup>
 import { useGetNumber } from "../../composables/useGetNumber";
-import { getTypeColor } from "../../composables/useGetTypeColor";
+import { useGetTypeColor } from "../../composables/useGetTypeColor";
 
-const { evaluation } = defineProps({
+const props = defineProps({
   data: {
     type: Object,
     required: true,
-    default: () => [],
   },
   evaluation: {
     type: Object,
     required: true,
-    default: () => [],
   },
   id: {
     type: String,
     required: true,
-    default: () => [],
   },
 });
 
+const { getTypeColor } = useGetTypeColor();
+
 const image = import.meta.env.VITE_URL_ASSET;
+
 const getEvolutions = (evolutionChain, evolutions = []) => {
   evolutions.push({
     name: evolutionChain?.species?.name,
@@ -43,58 +43,54 @@ const getEvolutions = (evolutionChain, evolutions = []) => {
       <div class="row">
         <div class="card">
           <div class="card-body text-center">
-            <div><img :src="image + id + '.svg'" alt="" /></div>
+            <div>
+              <img class="pokemon" :src="image + id + '.svg'" :alt="id" />
+            </div>
             <span>Nº {{ id }} </span>
 
             <h1>Estatísticas</h1>
             <div
-              class="d-flex justify-content-center flex-wrap text-center mb-5"
+              class="d-flex justify-content-center flex-wrap text-center mb-5 cursor-pointer"
             >
               <div
-                class="stats col-md-3"
+                class="stats col-md-4"
                 v-for="(status, i) in data?.stats"
                 :key="i"
               >
-                <span class="text-capitalize">
+                <span class="text-uppercase">
                   {{ status?.stat?.name }} {{ status?.base_stat }}
                 </span>
               </div>
             </div>
 
             <h1>Tipos</h1>
-
             <div class="d-flex justify-content-center flex-wrap mb-5">
               <div
-                class="stats col-md-3"
+                class="types text-white col-md-3"
                 v-for="(type, i) in data?.types"
                 :key="i"
-                :style="{
-                  'background-color': getTypeColor(type?.type?.name),
-                }"
+                :style="{ 'background-color': getTypeColor(type?.type?.name) }"
               >
                 <span class="text-capitalize">
-                  {{ type?.type?.name }} {{ getTypeColor(type?.type?.name) }}
+                  {{ type?.type?.name }}
                 </span>
               </div>
             </div>
 
             <h1 class="mb-4">Evoluções</h1>
-
             <div class="d-flex justify-content-center flex-wrap">
               <div
                 class="col-md-3 align-items-center"
                 v-for="(evolution, i) in getEvolutions(evaluation?.chain)"
                 :key="i"
               >
-                <div v-if="id != useGetNumber(evolution?.url)">
+                <div class="cursor-pointer">
                   <img
                     class="evolution"
                     :src="image + useGetNumber(evolution?.url) + '.svg'"
                     :alt="evolution?.url"
                   />
-                  <span class="text-capitalize">
-                    {{ evolution?.name }}
-                  </span>
+                  <span class="text-capitalize">{{ evolution?.name }}</span>
                 </div>
               </div>
             </div>
@@ -106,13 +102,25 @@ const getEvolutions = (evolutionChain, evolutions = []) => {
 </template>
 
 <style lang="sass" scoped>
+.pokemon
+  width: 150px
+  height: 100%
+
 .evolution
   width: 100px
   height: 100px
   object-fit: contain
 
 .stats
-  border: 1px solid #8EB0FF
+  background: #f5f5f5
+  border-radius: 18px
+  margin: 5px
+  padding: 10px 2px
+  font-size: 13px
+  &:hover
+    background-color: #F0C900
+    color: #fff
+.types
   border-radius: 18px
   margin: 2px
   padding: 13px 2px
